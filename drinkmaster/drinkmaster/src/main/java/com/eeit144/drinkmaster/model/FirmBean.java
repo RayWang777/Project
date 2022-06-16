3349d66ec6e5cc862f1142e55272741f2f665fa1
RW77 <ray128245213@gmail.com>
2022-06-16 20:52:42 +0800

    20220616_v0.8_finalproject_entity_pagecontroller_backendpage

---
+++ drinkmaster/drinkmaster/src/main/java/com/eeit144/drinkmaster/model/FirmBean.java
@@ -0,0 +1,86 @@
+package com.eeit144.drinkmaster.model;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "firm")
+public class FirmBean {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "firmid")
+	private Integer firmId;
+		
+	@Column(name = "firmname")
+	private String firmName;
+	
+	@Column(name = "firmaddress")
+	private String firmAddress;
+	
+	@Column(name = "firmPhone")
+	private String firmPhone;
+	
+	@Column(name = "firmlogo")
+	private byte[] firmLogo;
+	
+	@JsonIgnore
+	@OneToMany(fetch = FetchType.LAZY,mappedBy = "firmBean",cascade = CascadeType.ALL)
+	private Set<StoreBean> stores = new LinkedHashSet<StoreBean>();
+
+	public FirmBean() {
+	}
+
+	public Integer getFirmId() {
+		return firmId;
+	}
+
+	public void setFirmId(Integer firmId) {
+		this.firmId = firmId;
+	}
+
+	public String getFirmName() {
+		return firmName;
+	}
+
+	public void setFirmName(String firmName) {
+		this.firmName = firmName;
+	}
+
+	public String getFirmAddress() {
+		return firmAddress;
+	}
+
+	public void setFirmAddress(String firmAddress) {
+		this.firmAddress = firmAddress;
+	}
+
+	public String getFirmPhone() {
+		return firmPhone;
+	}
+
+	public void setFirmPhone(String firmPhone) {
+		this.firmPhone = firmPhone;
+	}
+
+	public byte[] getFirmLogo() {
+		return firmLogo;
+	}
+
+	public void setFirmLogo(byte[] firmLogo) {
+		this.firmLogo = firmLogo;
+	}
+
+}

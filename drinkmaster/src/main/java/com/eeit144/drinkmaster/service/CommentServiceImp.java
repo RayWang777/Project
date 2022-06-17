5db95f186772f61cb250f0204016bfa59df4a321
DESKTOP-NROIDJD\Student <qw>
2022-06-17 16:49:26 +0800

    controller

--- drinkmaster/src/main/java/com/eeit144/drinkmaster/service/CommentServiceImp.java
+++ drinkmaster/src/main/java/com/eeit144/drinkmaster/service/CommentServiceImp.java
@@ -3,21 +3,21 @@ package com.eeit144.drinkmaster.service;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
 
 import com.eeit144.drinkmaster.bean.CommentBean;
 import com.eeit144.drinkmaster.model.CommentService;
 
+@Service
 public class CommentServiceImp implements CommentService {
 
 	@Override
 	public Optional<CommentBean> findById(Integer id) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Page<CommentBean> findAll() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -25,12 +25,10 @@ public class CommentServiceImp implements CommentService {
 
 	@Override
 	public void deleteById(Integer id) {
-//retiun "sfsdfgsg";		
 	}
 
 	@Override
-	public void insertComment() {
-		// TODO Auto-generated method stub
+	public void insertComment(CommentBean comment) {
 		
 	}
 

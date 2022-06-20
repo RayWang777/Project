d42c416611aea39072eb07c7a74ef745c0a2205a
MSI\Alina <cdwewe456@gmail.com>
2022-06-20 10:05:02 +0800

    6/20 10:04 更新

--- drinkmaster/src/main/java/com/eeit144/drinkmaster/controller/ServiceController.java
+++ drinkmaster/src/main/java/com/eeit144/drinkmaster/controller/ServiceController.java
@@ -1,5 +1,15 @@
 package com.eeit144.drinkmaster.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.eeit144.drinkmaster.model.ServiceService;
+
+@Controller
 public class ServiceController {
 
+	@Autowired
+	private ServiceService service;
+	
+	
 }
--- drinkmaster/src/main/java/com/eeit144/drinkmaster/service/ServiceServiceImp.java
+++ drinkmaster/src/main/java/com/eeit144/drinkmaster/service/ServiceServiceImp.java
@@ -1,36 +1,55 @@
 package com.eeit144.drinkmaster.service;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.eeit144.drinkmaster.bean.ServiceBean;
+import com.eeit144.drinkmaster.dao.ServiceRepository;
 import com.eeit144.drinkmaster.model.ServiceService;
 
+
+@Service
+@Transactional
 public class ServiceServiceImp implements ServiceService {
 
+	@Autowired
+	private ServiceRepository serviceDao;
+	
 	@Override
-	public Optional<ServiceBean> findById(Integer id) {
-		// TODO Auto-generated method stub
+	public ServiceBean findById(Integer id) {
+		Optional<ServiceBean> optional = serviceDao.findById(id);
+		if(optional.isPresent()) {
+			return optional.get();
+		}
 		return null;
 	}
 
 	@Override
-	public Page<ServiceBean> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+	public Page<ServiceBean> findByPage(Integer pageNumber) {
+		Pageable pgb = PageRequest.of(pageNumber-1,3,Sort.Direction.DESC,"added");
+		Page<ServiceBean> findAll = serviceDao.findAll(pgb);
+		return findAll;
 	}
 
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		serviceDao.existsById(id);
 		
 	}
-
+	
 	@Override
-	public void insertService() {
-		// TODO Auto-generated method stub
+	public void insertService(ServiceBean service) {
+		serviceDao.save(service);
 		
 	}
 

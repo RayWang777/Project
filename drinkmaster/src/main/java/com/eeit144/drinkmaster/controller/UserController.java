caff4cb1ffb9c94b76567481b4467f91147ed338
RenZe <b14021116@gmail.com>
2022-06-20 13:49:28 +0800

    2022/06/20 修改order jsp bean的時間型態

--- drinkmaster/src/main/java/com/eeit144/drinkmaster/controller/UserController.java
+++ drinkmaster/src/main/java/com/eeit144/drinkmaster/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.eeit144.drinkmaster.bean.UserBean;
-import com.eeit144.drinkmaster.bean.UserBeanDTO;
+//import com.eeit144.drinkmaster.bean.UserBeanDTO;
 import com.eeit144.drinkmaster.model.UserService;
 
 @Controller
@@ -16,29 +16,29 @@ import com.eeit144.drinkmaster.model.UserService;
 
 public class UserController {
 
-	@Autowired
-	private UserService userService;
-	
-	@PostMapping("/user/insert")
-	public String insertUser(@RequestBody UserBeanDTO user, Model m) {
-		Integer userId = user.getUserId();
-		
-		System.out.println(userId);
-		
-		UserBean userBean = new UserBean();
-		
-		userBean.setUserName(user.getUserName());
-		userBean.setUserAccount(user.getUserAccount());
-		userBean.setUserPassword(user.getUserPassword());
-		userBean.setUserAddress(user.getUserAddress());
-		userBean.setPhone(user.getPhone());
-		userBean.setRole(user.getRole());
-		userBean.setGender(user.getGender());
-		userBean.setCreatedate(user.getCreatedate());
-		
-		userService.insertUser(userBean);
-		
-		return null;
-	}
+//	@Autowired
+//	private UserService userService;
+//	
+//	@PostMapping("/user/insert")
+//	public String insertUser(@RequestBody UserBeanDTO user, Model m) {
+//		Integer userId = user.getUserId();
+//		
+//		System.out.println(userId);
+//		
+//		UserBean userBean = new UserBean();
+//		
+//		userBean.setUserName(user.getUserName());
+//		userBean.setUserAccount(user.getUserAccount());
+//		userBean.setUserPassword(user.getUserPassword());
+//		userBean.setUserAddress(user.getUserAddress());
+//		userBean.setPhone(user.getPhone());
+//		userBean.setRole(user.getRole());
+//		userBean.setGender(user.getGender());
+//		userBean.setCreatedate(user.getCreatedate());
+//		
+//		userService.insertUser(userBean);
+//		
+//		return null;
+//	}
 	
 }

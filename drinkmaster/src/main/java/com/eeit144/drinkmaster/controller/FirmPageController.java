d1e6f2aebf8f93d9c80bdd15f9aa815031f69bb8
RW77 <ray128245213@gmail.com>
2022-06-21 20:32:03 +0800

    20220621

---
+++ drinkmaster/src/main/java/com/eeit144/drinkmaster/controller/FirmPageController.java
@@ -0,0 +1,61 @@
+package com.eeit144.drinkmaster.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.eeit144.drinkmaster.bean.FirmBean;
+import com.eeit144.drinkmaster.dto.FirmDTO;
+import com.eeit144.drinkmaster.model.FirmService;
+
+@Controller
+@RequestMapping("backend/")
+@SessionAttributes("firmsave")
+public class FirmPageController {
+
+	private FirmService firmService;
+
+	@Autowired
+	public FirmPageController(FirmService firmService) {
+		super();
+		this.firmService = firmService;
+	}
+
+	@GetMapping("firm/{id}")
+	public ResponseEntity<FirmDTO> findFirmById(@PathVariable Integer id) {
+		Optional<FirmBean> firmBean = firmService.findById(id);
+
+		if (firmBean.isEmpty()) {
+			return new ResponseEntity<FirmDTO>(HttpStatus.NO_CONTENT);
+		}
+		FirmDTO firmDTO = new FirmDTO();
+		firmDTO.setFirmId(firmBean.get().getFirmId());
+		firmDTO.setFirmName(firmBean.get().getFirmName());
+		firmDTO.setFirmAddress(firmBean.get().getFirmAddress());
+		firmDTO.setFirmPhone(firmBean.get().getFirmPhone());
+		firmDTO.setUserId(firmBean.get().getUserBean().getUserId());
+		return new ResponseEntity<FirmDTO>(firmDTO, HttpStatus.OK);
+	}
+
+	@GetMapping("firm/{id}/photo")
+	public ResponseEntity<byte[]> getFirmLogo(@PathVariable("id") Integer id) {
+		Optional<FirmBean> firmBean = firmService.findById(id);
+
+		byte[] firmLogo = firmBean.get().getFirmLogo();
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.IMAGE_JPEG);
+
+		return new ResponseEntity<byte[]>(firmLogo, headers, HttpStatus.OK);
+	}
+
+}

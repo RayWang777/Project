45078f6447d5818b35105bbc53673a7ae2572016
RW77 <ray128245213@gmail.com>
2022-07-05 15:44:40 +0800

    20220705 USER intercept

--- drinkmaster/src/main/java/com/eeit144/drinkmaster/front/config/InterceptorConfig.java
+++ drinkmaster/src/main/java/com/eeit144/drinkmaster/front/config/InterceptorConfig.java
@@ -21,7 +21,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(loginInterceptor).addPathPatterns("/front/**","/fornt/service/add")
-		.excludePathPatterns("/front/","/front/login**","/front/firm/**","/front/store/**","/front/localstore**","/front/comment/**","/front/productmenu","/front/service/FAQ","/front/chatroom");
+		.excludePathPatterns("/front/","/front/register**","/front/login**","/front/firm/**","/front/store/**","/front/localstore**","/front/comment/**","/front/productmenu","/front/service/FAQ","/front/chatroom");
 	}
 
 }
